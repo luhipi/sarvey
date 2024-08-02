2199db3b572e5a866cbe76799c1633b13be06697
mahmud1 <mahmud1@users.noreply.github.com>
2024-08-03 01:09:11 +0200

    implement selectP2 in densification

--- sarvey/densification.py
+++ sarvey/densification.py
@@ -28,17 +28,22 @@
 # with this program. If not, see <https://www.gnu.org/licenses/>.
 
 """Densification module for SARvey."""
+from os.path import join
 import time
 import multiprocessing
 import numpy as np
 from scipy.spatial import KDTree
 from logging import Logger
+import matplotlib.pyplot as plt
 
-from mintpy.utils import ptime
+from mintpy.utils import ptime, readfile
+from miaplpy.objects.slcStack import slcStack
 
 from sarvey.unwrapping import oneDimSearchTemporalCoherence
-from sarvey.objects import Points
+from sarvey.objects import Points, AmplitudeImage, BaseStack, ApsParameters
 import sarvey.utils as ut
+from sarvey.preparation import selectPixels, createTimeMaskFromDates
+from sarvey.filtering import applySimpleInterpolationToP2, applySpatialFilteringToP2
 
 
 def densificationInitializer(tree_p1: KDTree, tree_p2: KDTree, point2_obj: Points, demod_phase1: np.ndarray):
@@ -322,3 +327,236 @@ def densifyNetwork(*, point1_obj: Points, vel_p1: np.ndarray, demerr_p1: np.ndar
     gamma_p2 = gamma_p2[sort_idx]
     mean_gamma = mean_gamma[sort_idx]
     return demerr_p2, vel_p2, gamma_p2, mean_gamma
+
+
+def selectP2(*, output_path: str, config: dict, logger: Logger):
+    """Select second order points and interpolate APS to them
+
+    Parameters
+    ----------
+    output_path : str
+        path to the directory with processing files.
+    config : dict
+        dictionay containing parameters.
+    logger : Logger
+        Logger instance for logging messages.
+
+    Returns:
+    -------
+    point2_obj : Points
+        Points object with second-order points
+    aps2_obj: Points
+        Points object with APS estimation for second-order points
+
+    """
+    # this function is mainly adapted from sarvey.processing.runFiltering
+    # TODO: Directly pass input parameters instead of config dictionary
+    coherence_p2 = config.filtering.coherence_p2
+    logger.debug(f"Start Selecting 2nd order points with coherence threshold {coherence_p2:.2f}.")
+
+    coh_value = int(coherence_p2 * 100)
+
+    p1_file = join(output_path, "p1_ts_filt.h5")
+    logger.debug(f"Create a mask based on points in file {p1_file}.")
+    point1_obj = Points(file_path=p1_file, logger=logger)
+    point1_obj.open(path_inputs=config.data_directories.path_inputs)
+    p1_mask = point1_obj.createMask()  # used later for selecting psCand2 when a spatial mask AOI is given.
+    logger.debug(f"Mask created with {p1_mask.sum()} selected points.")
+
+    bmap_file = join(output_path, "background_map.h5")
+    logger.debug(f"Use file {bmap_file} to create a mask invalid points.")
+    bmap_obj = AmplitudeImage(file_path=bmap_file)
+
+    point_id_img = np.arange(0, point1_obj.length * point1_obj.width).reshape(
+        (point1_obj.length, point1_obj.width))
+
+    logger.debug(f"Select second-order points based on temporal coherence {coherence_p2:.2f}.")
+    cand_mask2 = selectPixels(
+        path=output_path, selection_method="temp_coh",
+        thrsh=coherence_p2,
+        grid_size=None, bool_plot=True,
+        logger=logger
+    )  # first-order points are included in second-order points
+    logger.debug(f"{cand_mask2.sum()} second-order points selected based on temporal coherence {coherence_p2:.2f}.")
+
+    if config.phase_linking.phase_linking:
+        # read PL results
+        pl_file = join(config.phase_linking.path_inverted, "phase_series.h5")
+        logger.debug(f"Read phase linking results from file {pl_file}.")
+        pl_coh = readfile.read(pl_file,
+                               datasetName='temporalCoherence')[0]
+        pl_coh = pl_coh[1, :, :]
+        siblings = readfile.read(pl_file,
+                                 datasetName='shp')[0]
+
+        if config.phase_linking.use_ps:
+            logger.debug(f"Read phase linking PS mask from file {config.phase_linking.path_mask_file_ps}.")
+            mask_ps = readfile.read(config.phase_linking.path_mask_file_ps,
+                                    datasetName='mask')[0].astype(np.bool_)
+            cand_mask_pl = (pl_coh > coherence_p2) | mask_ps
+        else:
+            cand_mask_pl = (pl_coh > coherence_p2)
+            # remove ps candidates, because the ps detection strategy in miaplpy seems to be biased.
+            cand_mask_pl[siblings <= config.phase_linking.num_siblings] = False
+
+        if config.phase_linking.spatial_mask_file_pl is not None:
+            path_mask_pl_aoi = join(config.phase_linking.spatial_mask_file_pl)
+            logger.debug(f"Load mask for area of interest from file {path_mask_pl_aoi}.")
+            mask_pl_aoi = readfile.read(path_mask_pl_aoi, datasetName='mask')[0].astype(np.bool_)
+
+            fig = plt.figure(figsize=[15, 5])
+            ax = fig.add_subplot()
+            ax.imshow(mask_pl_aoi, cmap=plt.cm.get_cmap("gray"), alpha=0.5, zorder=10, vmin=0, vmax=1)
+            bmap_obj.plot(ax=ax, logger=logger)
+            coord_xy = np.array(np.where(cand_mask_pl)).transpose()
+            val = np.ones_like(cand_mask_pl)
+            sc = ax.scatter(coord_xy[:, 1], coord_xy[:, 0], c=val[cand_mask_pl], s=0.5,
+                            cmap=plt.get_cmap("autumn_r"),
+                            vmin=1, vmax=2)  # set min, max to ensure that points are yellow
+            cbar = plt.colorbar(sc, pad=0.03, shrink=0.5)
+            cbar.ax.set_visible(False)  # make size of axis consistent with all others
+            plt.tight_layout()
+            plt.title("Mask for phase linking points")
+            fig.savefig(join(output_path, "pic", "step_3_mask_coh{}_phase_linking.png".format(coh_value)), dpi=300)
+            plt.close(fig)
+
+            # mask points after plotting, so that available coherent points are visible in figure
+            cand_mask_pl[~mask_pl_aoi] = False
+
+        # combine phase linking coherence with TPC cand_mask2
+        initial_num_cand_mask2 = cand_mask2.sum()
+        cand_mask2 = cand_mask2 | cand_mask_pl
+        logger.debug(f"{cand_mask2.sum()-initial_num_cand_mask2} additional pixels selected by phase linking.")
+
+    mask_valid_area = ut.detectValidAreas(bmap_obj=bmap_obj, logger=logger)
+
+    if config.filtering.spatial_mask_file_p2 is not None:
+        path_mask_aoi = join(config.filtering.spatial_mask_file_p2)
+        logger.info(f"Load mask for area of interest from file {path_mask_aoi}.")
+        mask_aoi = readfile.read(path_mask_aoi, datasetName='mask')[0].astype(np.bool_)
+        mask_valid_area &= mask_aoi
+        # todo: add unstable points from p1 for densification
+    else:
+        logger.info(msg="No mask for area of interest given.")
+
+    cand_mask2[p1_mask] = True  # add all selected 1.order points to avoid spatial gaps in 2D unwrapping
+    # cand_mask2[cand_mask_sparse] = True  # add only stable points from 1.order points
+
+    cand_mask2 &= mask_valid_area
+    logger.info(f"Final number of selected second-order points: {cand_mask2.sum()}.")
+
+    fig = plt.figure(figsize=[15, 5])
+    ax = fig.add_subplot()
+    ax.imshow(mask_valid_area, cmap=plt.cm.get_cmap("gray"), alpha=0.5, zorder=10, vmin=0, vmax=1)
+    bmap_obj.plot(ax=ax, logger=logger)
+    coord_xy = np.array(np.where(cand_mask2)).transpose()
+    val = np.ones_like(cand_mask2)
+    sc = ax.scatter(coord_xy[:, 1], coord_xy[:, 0], c=val[cand_mask2], s=0.5, cmap=plt.get_cmap("autumn_r"),
+                    vmin=1, vmax=2)  # set min, max to ensure that points are yellow
+    cbar = plt.colorbar(sc, pad=0.03, shrink=0.5)
+    cbar.ax.set_visible(False)  # make size of axis consistent with all others
+    plt.tight_layout()
+    plt.title("Mask for dense point set")
+    fig.savefig(join(output_path, "pic", "step_3_mask_coh{}.png".format(coh_value)), dpi=300)
+    plt.close(fig)
+
+    p2_file = join(output_path, f"coh{coh_value}_ifg_wr.h5")
+    logger.debug(f"Prepare second-order points object to save to file {p2_file}.")
+    point2_obj = Points(file_path=p2_file, logger=logger)
+    coord_xy = np.array(np.where(cand_mask2)).transpose()
+    point_id2 = point_id_img[cand_mask2]
+    point2_obj.prepare(
+        point_id=point_id2,
+        coord_xy=coord_xy,
+        path_inputs=config.data_directories.path_inputs
+    )
+
+    ifg_stack_file = join(output_path, "ifg_stack.h5")
+    logger.debug(f"Read interferogram phase for selected second-order points from file {ifg_stack_file}.")
+    ifg_stack_obj = BaseStack(file=ifg_stack_file, logger=logger)
+    point2_obj.phase = ut.readPhasePatchwise(stack_obj=ifg_stack_obj, dataset_name="ifgs",
+                                             num_patches=config.processing.num_patches, cand_mask=cand_mask2,
+                                             point_id_img=point_id_img, logger=logger)
+
+    if config.phase_linking.phase_linking:
+        pl_inverted_file = join(config.phase_linking.path_inverted, "phase_series.h5")
+        logger.debug(f"Read interferogram phase from MiaplPy results from file {pl_inverted_file}.")
+        phase_linking_obj = BaseStack(file=pl_inverted_file,
+                                      logger=logger)
+
+        pl_phase = ut.readPhasePatchwise(
+            stack_obj=phase_linking_obj, dataset_name="phase",
+            num_patches=config.processing.num_patches,
+            cand_mask=cand_mask2,
+            point_id_img=point_id_img, logger=logger
+        )
+
+        # subset to time span
+        slc_stack_obj = slcStack(join(config.data_directories.path_inputs, "slcStack.h5"))
+        slc_stack_obj.open()
+        time_mask = createTimeMaskFromDates(
+            start_date=config.preparation.start_date,
+            stop_date=config.preparation.stop_date,
+            date_list=slc_stack_obj.dateList,
+            logger=logger
+        )[0]
+        pl_phase = pl_phase[:, time_mask]
+
+        pl_ifgs = np.zeros((point2_obj.num_points, point2_obj.ifg_net_obj.num_ifgs), dtype=np.float32)
+
+        c = 0
+        for i, j in np.asarray(point1_obj.ifg_net_obj.ifg_list):
+            pl_ifgs[:, c] = np.angle(np.exp(1j * pl_phase[:, i]) * np.conjugate(np.exp(1j * pl_phase[:, j])))
+            c += 1
+
+        # change only phase for good phase linking pixels and keep original phase for good tpc pixels
+        mask_pl = cand_mask_pl[cand_mask2]
+        point2_obj.phase[mask_pl] = pl_ifgs[mask_pl]
+
+    logger.debug(f"Write second-order points to file {point2_obj.file_path}.")
+    point2_obj.writeToFile()
+    del ifg_stack_obj
+
+    p1_aps_file = join(output_path, "p1_aps.h5")
+    logger.debug(f"Read APS for first order point from file {p1_aps_file}.")
+    aps1_obj = Points(file_path=p1_aps_file, logger=logger)
+    aps1_obj.open(path_inputs=config.data_directories.path_inputs)
+
+    aps_params_file = join(output_path, "aps_parameters.h5")
+    logger.debug(f"Read APS parameters from file {aps_params_file}.")
+    aps_params_obj = ApsParameters(file_path=aps_params_file, logger=logger)
+    aps_params_obj.open()
+
+    if config.filtering.skip_filtering:
+        logger.info("Skip APS filtering")
+        # TODO: check if this is correct ####
+        # original code: aps2_phase = np.zeros_like(point2_obj.phase)
+        aps2_phase = np.zeros((point2_obj.num_points, aps_params_obj.phase.shape[1]))
+    else:
+        logger.debug("Apply APS filtering to second-order points.")
+        if config.filtering.interpolation_method == "kriging":
+            aps2_phase = applySpatialFilteringToP2(coord_utm1=aps1_obj.coord_utm,
+                                                   residuals=aps_params_obj.phase,
+                                                   coord_utm2=point2_obj.coord_utm,
+                                                   model_name=aps_params_obj.model_name,
+                                                   model_params=aps_params_obj.model_params,
+                                                   logger=logger)
+        else:
+            aps2_phase = applySimpleInterpolationToP2(residuals=aps_params_obj.phase,
+                                                      coord_utm1=aps1_obj.coord_utm,
+                                                      coord_utm2=point2_obj.coord_utm,
+                                                      logger=logger,
+                                                      interp_method=config.filtering.interpolation_method)
+
+    p2_aps_file = join(output_path, f"coh{coh_value}_aps.h5")
+    logger.info(f"Write APS for second-order points to file {p2_aps_file}.")
+    aps2_obj = Points(file_path=p2_aps_file, logger=logger)
+    aps2_obj.open(
+        other_file_path=join(output_path, f"coh{coh_value}_ifg_wr.h5"),
+        path_inputs=config.data_directories.path_inputs
+    )
+    aps2_obj.phase = aps2_phase
+    aps2_obj.writeToFile()
+
+    logger.debug("Finished selecting second-order points.")
+    return point2_obj, aps2_obj
