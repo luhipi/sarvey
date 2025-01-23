5934d729bdaa7c66898b6523273bb3cdee50762f
Mahmud Haghighi <mahmud1@users.noreply.github.com>
2025-01-23 19:20:12 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'classic'  # The one installed via pip install sphinx_rtd_theme in the .gitlab.yml
+html_theme = 'sphinx_rtd_theme'  # The one installed via pip install sphinx_rtd_theme in the .gitlab.yml
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
